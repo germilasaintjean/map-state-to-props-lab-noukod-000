77828f5371f730b2ea92a86f2142df87029d395e
Saint-Jean Germila <germila@noukod.com>
2019-04-30 14:23:24 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux'; /* code change */
-import App from './App';
-import './index.css';
-
+import { Provider } from 'react-redux';
+import App from './App'
 
 import manageUsers from './reducers/manageUsers'
 
 const store = createStore(
-managgeUsers,
- window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  manageUsers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(
- <Provider store={store}>
-   <App />
- </Provider>, /* code change */
- document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
