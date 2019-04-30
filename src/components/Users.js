bc590ded6b69455a407ed2009fb13a765ea1d034
Saint-Jean Germila <germila@noukod.com>
2019-04-30 14:24:13 +0000

    Automatically backed up by Learn

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,21 +4,26 @@ import { connect } from 'react-redux';
 class Users extends Component {
 
   render() {
+    let users = this.props.users.map((user, index) =>
+    <li key={index}>{user.username}</li>
+    )
+
     return (
       <div>
+        <h3>Users: {this.props.users.length}</h3>
         <ul>
-          Users!
+          {users}
         </ul>
       </div>
     )
   }
 }
 
-//add mapStateToProps here
 const mapStateToProps = (state) => {
   console.log(state)
-  return { users: state.users }
+  return {users: state.users }
 }
 
+//add mapStateToProps here
 
-export default connect (mapStateToProps) (Users);
+export default connect(mapStateToProps)(Users);
